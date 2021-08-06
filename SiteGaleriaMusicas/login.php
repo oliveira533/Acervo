dea3788c935a0b7b85c1867a8bed0b6f28e236f4
CauaRojas <84928610+CauaRojas@users.noreply.github.com>
2021-08-06 18:14:39 -0300

    Add files via upload

---
+++ SiteGaleriaMusicas/login.php
@@ -0,0 +1,18 @@
+<?php
+$oCon = mysqli_connect('localhost', 'Aluno2DS', 'SenhaBD2', 'BANCOCOMUM');
+$cSQL = "SELECT USRCODIGO, USRNOME" .
+        "  FROM USUARIOS" .
+		" WHERE '" . $_GET['txtLogin'] . "' IN (USRLOGIN, USREMAIL)" .
+		"   AND USRSENHA = MD5('" . $_GET['txtSenha'] . "')";
+$oUsuarios = mysqli_query($oCon, $cSQL);
+
+//echo $cSQL;
+
+if($vReg = mysqli_fetch_assoc($oUsuarios))
+	echo $vReg['USRNOME'];
+else
+	echo 'Usuário ou senha incorretos';
+
+mysqli_free_result($oUsuarios);
+mysqli_close($oCon);
+?>
