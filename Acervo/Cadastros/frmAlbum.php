4b0a54f990ad34ebe552db8b58457e107fbbd76e
CauaRojas <cauarojas@hotmail.com>
2021-08-14 09:36:22 -0300

    Extrai o arquivo

---
+++ Acervo/Cadastros/frmAlbum.php
@@ -0,0 +1,3 @@
+<html>
+    <form action="../cadastrar.php"></form>
+</html>
---
+++ Acervo/login.php
@@ -0,0 +1,16 @@
+<?php
+$conexao = mysqli_connect("localhost","Aluno2DS","SenhaBD2","BANCOCOMUM");
+$cSQL = "SELECT USRCODIGO, USRNOME" .
+        "  FROM USUARIOS" .
+        " WHERE '" . $_GET['txbEmail'] . "' IN (USRLOGIN, USREMAIL)" .
+        "   AND USRSENHA = MD5('" . $_GET['txbSenha'] . "')";
+$oUsuarios = mysqli_query($conexao, $cSQL);
+
+if($vReg = mysqli_fetch_assoc($oUsuarios))
+	echo $vReg['USRNOME'];
+else
+	echo 'Usuário ou senha incorretos';
+
+mysqli_free_result($oUsuarios);
+mysqli_close($conexao);
+?>
