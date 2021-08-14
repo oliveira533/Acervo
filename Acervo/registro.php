735e9ae5695de027a819e54cc75c36eafa65d0e7
Var-Argas <84928747+Var-Argas@users.noreply.github.com>
2021-08-14 13:19:14 -0300

    Update registro.php

--- Acervo/registro.php
+++ Acervo/registro.php
@@ -4,4 +4,5 @@
     $sql = "INSERT INTO USUARIOS (USRNOME, USRLOGIN, USREMAIL,USRSENHA) VALUES ('" . $_GET['txbUser'] . "', '". $_GET['txbLogin']."', '". $_GET['txbEmail']."',md5('".$_GET['txbSenha']."'))";
     mysqli_query($conexao,$sql);
     header("location:login.php?txbEmail=". $_GET['txbLogin'] . "&txbSenha=" . $_GET['txbSenha']);
+  
 ?>
