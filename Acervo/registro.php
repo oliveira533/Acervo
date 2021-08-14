40df33c746926f97b36df468bb76d4dcff7d7d67
CauaRojas <cauarojas@hotmail.com>
2021-08-14 12:47:12 -0300

    Adicionado a função de registro

---
+++ Acervo/registro.php
@@ -0,0 +1,7 @@
+<?php 
+    session_start();
+    $conexao = mysqli_connect("localhost","root","","BANCOCOMUM");
+    $sql = "INSERT INTO USUARIOS (USRNOME, USRLOGIN, USREMAIL,USRSENHA) VALUES ('" . $_GET['txbUser'] . "', '". $_GET['txbLogin']."', '". $_GET['txbEmail']."',md5('".$_GET['txbSenha']."'))";
+    mysqli_query($conexao,$sql);
+    header("location:login.php?txbEmail=". $_GET['txbLogin'] . "&txbSenha=" . $_GET['txbSenha']);
+?>
