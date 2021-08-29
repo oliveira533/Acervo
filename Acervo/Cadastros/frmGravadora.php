94c4cfe13dc585f4dcd4dd3720d17123cfd5ceaf
CauaRojas <cauarojas@hotmail.com>
2021-08-29 14:34:34 -0300

    Update frmGravadora.php

--- Acervo/Cadastros/frmGravadora.php
+++ Acervo/Cadastros/frmGravadora.php
@@ -2,7 +2,7 @@
 <body>
 <form action="./Cadastros/cadastrar.php">
 <label for="txbNome">Nome</label><input type="text" name="txbNome" id="txbNome"><br>
-<label for="TxbInicio">Data de Inalguração da Gravadora</label><input type="date" name="txbInicio">
+<label for="TxbInicio">Data de Inauguração da Gravadora</label><input type="date" name="txbInicio">
 <label for="TxbFinal">Data de Falimento da Gravadora</label><input type="date" name="txbFinal">
 </body>
 </html>
