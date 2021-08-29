28a1c0c2056e00d3e8415d22d4ff125eeed1b208
Var-Argas <84928747+Var-Argas@users.noreply.github.com>
2021-08-29 16:47:16 -0300

    usuario css

--- Acervo/usuario.php
+++ Acervo/usuario.php
@@ -10,7 +10,7 @@
     <body>
         <nav class="menu aaa">
             <ul>
-                <li><a href="#" style="color: #333;"><img src="#"></a></li>
+                <li><a href="#" style="color: #333;"><img class="imgusu"src="#"></a></li>
                 <li><a href="cadastro.php" style="color: #333;"><button>Inserir conteúdo</button></a></li>
                 <li><a href="https://c.tenor.com/MhIARALUqvAAAAAd/ednaldo-pereira-vale-nada.gif" style="color: #333;"><button>Alterar dados</button></a></li>
             </ul>
@@ -18,26 +18,26 @@
         <div class="Musica tudo">
             <label class="Titulo">Músicas que você cadastrou</label><br>
             <div class="card">
-                <img src="#"><br>
+                <img class="imgbox" src="#"><br>
                 <label>Musica</label>
             </div>        
         </div>
         <div class="Banda tudo">
             <label class="Titulo">Bandas que você cadastrou</label><br>
             <div class="card">
-                <img src="#"><br>
+                <img class="imgbox" src="#"><br>
                 <label>Banda</label>
             </div>        
         </div>
         <div class="Album tudo">
             <label class="Titulo">Albuns que você cadastrou</label><br>
             <div class="card">
-                <img src="#"><br>
+                <img  class="imgbox"src="#">
                 <label>Album</label>
             </div>        
         </div>
         <a href="index.php">
-			<img src="./images/home.png" >
+			<img class="imghome"src="./images/home.png" >
 		</a>
     </body>
 </html>
