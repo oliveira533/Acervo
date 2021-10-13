921b04e8767fe052e4c4c2195a0cd494d4224b13
CauaRojas <cauarojas@hotmail.com>
2021-10-13 18:54:03 -0300

    Update busca.php

--- Acervo/busca.php
+++ Acervo/busca.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Busca: <?php $_GET["txbPesquisa"] ?></title>
+    <title>Busca: <?php echo $_GET["txbPesquisa"] ?></title>
 </head>
 
 <body>
